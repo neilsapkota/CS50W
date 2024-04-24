abd8677961b073efcd95c9ea41c04801a1db9434
Neil Sapkota <neilsapkota@gmail.com>
2024-04-24 19:23:17 +0545

    Update admin.py

--- Capstone /pokhara/admin.py
+++ Capstone /pokhara/admin.py
@@ -1,1 +1,12 @@
+from django.contrib import admin
+from .models import  User, Posts, Like, Comment
+
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "content", "time", "like")
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Posts, PostsAdmin)
+admin.site.register(Like)
+admin.site.register(Comment)
 
